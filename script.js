98231e7b2177b750a07e5b6e3b579afa3150787b
shlomtzionm <shlomtzosh@gmail.com>
2024-04-17 15:48:58 +0300

    structure

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-document.getElementById('fileInput').addEventListener('change', function(e) {
+const modifiedJsonData = {};
+
+function getStoreName(jsonData) {
+    for (let i = 2; i < jsonData.length - 1; i++) {
+        const row = jsonData[i];
+        const value = row[0];
+        modifiedJsonData[value] = {};
+    }
+}
+
+function getDate(jsonData) {
+    const dates = [];
+    // Iterate over the first row (excluding the first cell)
+    for (let i = 0; i < jsonData[0].length; i++) {
+        const excelDateValue = jsonData[0][i]; 
+    
+        const jsDate = new Date((excelDateValue - (25567 + 1)) * 86400 * 1000);
+        const formattedDate = moment(jsDate).format('YYYY-MM-DD');
+        dates.push(formattedDate);
+ 
+    }
+    return dates;
+}
+
+function handel(e) {
     const file = e.target.files[0];
     const reader = new FileReader();
 
@@ -10,18 +34,21 @@ document.getElementById('fileInput').addEventListener('change', function(e) {
       
         const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-        const modifiedJsonData = {};
-       
-        for (let i = 2; i < jsonData.length - 1; i++) {
-            const row = jsonData[i];
-            const value = row[0];
-            
-            
-            modifiedJsonData[value] = 0
+        getStoreName(jsonData);
+        const dates = getDate(jsonData);
+
+        // Assign dates to modifiedJsonData for each store
+        for (let store in modifiedJsonData) {
+            for (let i = 0; i < dates.length; i++) {
+                modifiedJsonData[store][dates[i]] = 0;
+           
+            }
         }
 
         console.log(modifiedJsonData);
     };
 
     reader.readAsArrayBuffer(file);
-});
+}
+
+document.getElementById('fileInput').addEventListener('change', handel);
