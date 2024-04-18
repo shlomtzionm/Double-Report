de3e11869d738570284246c2d6970d172a58cbb9
shlomtzionm <shlomtzosh@gmail.com>
2024-04-18 13:08:18 +0300

    fix: strucuer

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ function getStoreName(jsonData) {
     for (let i = 2; i < jsonData.length - 1; i++) {
         const row = jsonData[i];
         const value = row[0];
-        modifiedJsonData[value] = {};
+        modifiedJsonData[value] = { index: i };
     }
 }
 
 function getDate(jsonData) {
     const dates = [];
-    // Iterate over the first row (excluding the first cell)
+
     for (let i = 0; i < jsonData[0].length; i++) {
-        const excelDateValue = jsonData[0][i]; 
-    
-        const jsDate = new Date((excelDateValue - (25567 + 1)) * 86400 * 1000);
+        const excelDateValue = jsonData[0][i];
+
+        const jsDate = new Date((excelDateValue - (25567 + 2)) * 86400 * 1000);
         const formattedDate = moment(jsDate).format('YYYY-MM-DD');
-        dates.push(formattedDate);
- 
+
+        if (formattedDate !== "Invalid date") {
+            dates.push(formattedDate);
+        }
     }
+
     return dates;
 }
 
@@ -31,19 +34,29 @@ function handel(e) {
         const workbook = XLSX.read(data, { type: 'array' });
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
-      
+
         const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
         getStoreName(jsonData);
         const dates = getDate(jsonData);
 
-        // Assign dates to modifiedJsonData for each store
         for (let store in modifiedJsonData) {
-            for (let i = 0; i < dates.length; i++) {
-                modifiedJsonData[store][dates[i]] = 0;
-           
+            const storeIndex = modifiedJsonData[store].index;
+            modifiedJsonData[store]["data"] = {}; // Create an object to store data for each store
+        
+            for (let j = 0; j < dates.length; j++) {
+                const currentDate = dates[j];
+                modifiedJsonData[store]["data"][currentDate] = {
+                    "תקן": jsonData[storeIndex][1 + 3 * j],
+                    "ביצוע": jsonData[storeIndex][2 + 3 * j],
+                    "פער": jsonData[storeIndex][3 + 3 * j]
+                };
             }
         }
+        
+        
+        
+        
 
         console.log(modifiedJsonData);
     };
