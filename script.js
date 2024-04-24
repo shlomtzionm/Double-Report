89f2ab34d9f86ec0464296625ed3187ec5a293fb
shlomtzionm <shlomtzosh@gmail.com>
2024-04-24 15:21:53 +0300

    24.04

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-const modifiedJsonData = {};
 
-function getStoreName(jsonData) {
-    for (let i = 2; i < jsonData.length - 1; i++) {
-        const row = jsonData[i];
-        const value = row[0];
-        modifiedJsonData[value] = { index: i };
-    }
-}
 
-function getDate(jsonData) {
-    const dates = [];
+const moreThen2 = {};
+const moreThen5 = {};
 
-    for (let i = 0; i < jsonData[0].length; i++) {
-        const excelDateValue = jsonData[0][i];
-
-        const jsDate = new Date((excelDateValue - (25567 + 2)) * 86400 * 1000);
-        const formattedDate = moment(jsDate).format('YYYY-MM-DD');
-
-        if (formattedDate !== "Invalid date") {
-            dates.push(formattedDate);
-        }
-    }
-
-    return dates;
-}
+document.getElementById('fileInput').addEventListener('change', handel);
 
 function handel(e) {
     const file = e.target.files[0];
@@ -36,32 +16,118 @@ function handel(e) {
         const sheet = workbook.Sheets[sheetName];
 
         const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        const dates = getDate(jsonData); 
 
-        getStoreName(jsonData);
-        const dates = getDate(jsonData);
+        buildObjects(jsonData, dates); 
 
-        for (let store in modifiedJsonData) {
-            const storeIndex = modifiedJsonData[store].index;
-            modifiedJsonData[store]["data"] = {}; // Create an object to store data for each store
+        console.log(moreThen2);
+        console.log(moreThen5);
         
-            for (let j = 0; j < dates.length; j++) {
-                const currentDate = dates[j];
-                modifiedJsonData[store]["data"][currentDate] = {
-                    "תקן": jsonData[storeIndex][1 + 3 * j],
-                    "ביצוע": jsonData[storeIndex][2 + 3 * j],
-                    "פער": jsonData[storeIndex][3 + 3 * j]
-                };
-            }
-        }
-        
-        
-        
-        
-
-        console.log(modifiedJsonData);
     };
 
     reader.readAsArrayBuffer(file);
 }
 
-document.getElementById('fileInput').addEventListener('change', handel);
+function buildObjects(jsonData, dates) {
+    for (let i = 5; i < jsonData.length; i++) {
+        console.log('Current row:', jsonData[i]);
+
+        const row = jsonData[i];
+        const value = row[0];
+
+        for (let j = 4; j < dates.length; j++) {
+            const currentDate = dates[j];
+            const standardIndex = 1 + 3 * j;
+          
+            const performance = jsonData[i][standardIndex + 1];
+            const gap = jsonData[i][standardIndex + 2];
+
+            console.log('Performance:', performance);
+            console.log('Gap:', gap);
+
+            if ((jsonData[i][standardIndex] !== undefined) && (gap < -2)) {
+                let data = { [currentDate]: {
+                            "תקן": jsonData[i][standardIndex],
+                            "ביצוע": performance,
+                            "פער": gap
+                        }
+                };
+                moreThen2[value] = data;
+            } else if ((jsonData[i][standardIndex] === undefined) && (gap < -5)) {
+                let data= {[currentDate]: {
+                            "תקן": undefined,
+                            "ביצוע": performance,
+                            "פער": gap
+                        }
+                    
+                };
+                moreThen5[value] = data;
+            }
+        }
+    }
+}
+
+
+
+
+
+function getDate(jsonData) {
+    const dates = [];
+    for (let i = 3; i < jsonData[0].length; i++) {
+        const excelDateValue = jsonData[0][i];
+        const jsDate = new Date((excelDateValue - (25567 + 2)) * 86400 * 1000);
+        const formattedDate = moment(jsDate).format('YYYY-MM-DD');
+        if (formattedDate !== "Invalid date") {
+            dates.push(formattedDate);
+        }
+    }
+    return dates;
+}
+
+
+
+
+
+
+
+
+document.querySelector("#downloadExcel").addEventListener("click",creatNewExcel)
+
+function creatNewExcel() {
+    let headers = ["שם מקור דיווח ", "תאריך", "תקן", "ביצוע", "פער"];
+    let combinedData = [headers];
+    let combinedData5 = [headers];
+
+    buildRows(moreThen2, combinedData);
+    buildRows(moreThen5, combinedData5);
+console.log(combinedData)
+console.log(combinedData5)
+    const workbook = XLSX.utils.book_new();
+
+    const worksheet = XLSX.utils.aoa_to_sheet(combinedData);
+    const worksheet5 = XLSX.utils.aoa_to_sheet(combinedData5);
+
+    XLSX.utils.book_append_sheet(workbook, worksheet, '2');
+    XLSX.utils.book_append_sheet(workbook, worksheet5, '5');
+
+    // Save the workbook to a file
+    XLSX.writeFile(workbook, 'output.xlsx');
+
+    console.log("Data has been successfully exported to output.xlsx");
+}
+
+function buildRows(fiveOrTow, array) {
+    for (let store in fiveOrTow) {
+        for (let date in fiveOrTow[store]) {
+            const dateData = fiveOrTow[store][date];
+            const rowData = [
+                store,
+                date,
+                dateData["תקן"],
+                dateData["ביצוע"],
+                dateData["פער"]
+            ];
+            array.push(rowData);
+        }
+    }
+}
