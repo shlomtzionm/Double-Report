e611bc31a65d3c06abb239ee68fb2f143b142278
shlomtzion mendlovich <shlomtzosh@gmail.com>
2024-04-28 15:37:09 +0300

    28.04

--- 111.js
+++ 111.js
@@ -1,67 +1,125 @@
-const modifiedJsonData = {};
+const moreThen2 = {};
+const moreThen5 = {};
 
-function getStoreName(jsonData) {
-    for (let i = 2; i < jsonData.length - 1; i++) {
-        const row = jsonData[i];
-        const value = row[0];
-        modifiedJsonData[value] = { index: i };
-    }
-}
+document.getElementById('fileInput').addEventListener('change', handle);
 
-function getDate(jsonData) {
-    const dates = [];
-
-    for (let i = 0; i < jsonData[0].length; i++) {
-        const excelDateValue = jsonData[0][i];
-
-        const jsDate = new Date((excelDateValue - (25567 + 2)) * 86400 * 1000);
-        const formattedDate = moment(jsDate).format('YYYY-MM-DD');
-
-        if (formattedDate !== "Invalid date") {
-            dates.push(formattedDate);
-        }
-    }
-
-    return dates;
-}
-
-function handel(e) {
+function handle(e) {
     const file = e.target.files[0];
     const reader = new FileReader();
 
-    reader.onload = function(event) {
+    reader.onload = function (event) {
         const data = new Uint8Array(event.target.result);
         const workbook = XLSX.read(data, { type: 'array' });
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
 
         const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-
-        getStoreName(jsonData);
         const dates = getDate(jsonData);
+       
+        buildObjects(jsonData, dates);
 
-        for (let store in modifiedJsonData) {
-            const storeIndex = modifiedJsonData[store].index;
-            modifiedJsonData[store]["data"] = {}; // Create an object to store data for each store
-        
-            for (let j = 0; j < dates.length; j++) {
-                const currentDate = dates[j];
-                modifiedJsonData[store]["data"][currentDate] = {
-                    "תקן": jsonData[storeIndex][3 + 3 * j],
-                    "ביצוע": jsonData[storeIndex][4 + 3 * j],
-                    "פער": jsonData[storeIndex][5 + 3 * j]
-                };
-            }
-        }
-        
-        
-        
-        
-
-        console.log(modifiedJsonData);
+        console.log(moreThen2);
+        console.log(moreThen5);
     };
 
     reader.readAsArrayBuffer(file);
 }
 
-document.getElementById('fileInput').addEventListener('change', handel);
+function buildObjects(jsonData, dates) {
+    for (let i = 5; i < jsonData.length; i++) {
+        const row = jsonData[i];
+        const value = row[0];
+        const manager = row[1];
+
+        for (let j = 0; j < dates.length; j++) {
+            const currentDate = dates[j];
+            const performance = jsonData[i][j *3 + 4];
+            const gap = jsonData[i][j*3  + 5];
+
+            if ((jsonData[i][j+ 3] !== undefined) && (gap < -2)) {
+                let data = {
+                    "תקן": jsonData[i][j*3 + 3],
+                    "ביצוע": performance,
+                    "פער": gap
+                };
+               
+                    moreThen2[value] = {};
+                
+                moreThen2[value][currentDate] = data;
+                moreThen2[value]["מנהל"]= manager
+              
+            } 
+             if ((jsonData[i][j *3 + 3] === undefined ) && (gap < -5)) {
+                let data = {
+                    "תקן": "",
+                    "ביצוע": performance,
+                    "פער": gap,
+                };
+                if (!moreThen5[value]) {
+                    moreThen5[value] = {};
+                }
+                moreThen5[value][currentDate] = data;
+            moreThen5[value]["מנהל"] = manager
+       
+            }
+        }
+    }
+}
+
+function getDate(jsonData) {
+    const dates = [];
+    for (let i = 3; i < jsonData[3].length; i++) {
+        const excelDateValue = jsonData[3][i];
+        const jsDate = new Date((excelDateValue - (25567 + 2)) * 86400 * 1000);
+        const formattedDate = moment(jsDate).format('YYYY-MM-DD');
+        if (formattedDate !== "Invalid date") {
+            dates.push(formattedDate);
+        }
+    }
+    return dates;
+}
+
+document.querySelector("#downloadExcel").addEventListener("click", createNewExcel);
+
+function createNewExcel() {
+    let headers = ["שם מקור דיווח ", "תאריך", "תקן", "ביצוע", "פער"];
+    let combinedData = [headers];
+    let combinedData5 = [headers];
+
+    buildRows(moreThen2, combinedData);
+    buildRows(moreThen5, combinedData5);
+    console.log(combinedData);
+    console.log(combinedData5);
+    const workbook = XLSX.utils.book_new();
+
+    const worksheet = XLSX.utils.aoa_to_sheet(combinedData);
+    const worksheet5 = XLSX.utils.aoa_to_sheet(combinedData5);
+
+    XLSX.utils.book_append_sheet(workbook, worksheet, '2');
+    XLSX.utils.book_append_sheet(workbook, worksheet5, '5');
+
+    // Save the workbook to a file
+    XLSX.writeFile(workbook, 'output.xlsx');
+
+    console.log("Data has been successfully exported to output.xlsx");
+}
+
+function buildRows(fiveOrTwo, array) {
+    for (let store in fiveOrTwo) {
+        for (let date in fiveOrTwo[store]) {
+            if(date !== "מנהל"){
+                const dateData = fiveOrTwo[store][date];
+                const rowData = [
+                    store,
+                    date,
+                    dateData["תקן"],
+                    dateData["ביצוע"],
+                    dateData["פער"],
+                    fiveOrTwo[store]["מנהל"]
+                ];
+                array.push(rowData);
+        
+            }
+        }
+    }
+}
